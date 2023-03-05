97f5a31c677945e0e4581bd2a0c3d20dd971f7fe
Silvino Aguiar <2121100431@soy.utj.edu.mx>
2023-03-05 16:24:54 -0600

    registro de usuario

---
+++ usuario/index.php
@@ -0,0 +1,45 @@
+<?php
+include('../config.php');
+include('./usuario.php');
+
+try {
+    $accion = "No se encontro ninguna accion";
+    $result = false;
+
+    $usuario = new Usuario();
+    if(isset($input['accion'])){
+        $accion = $input['accion'];
+        $usuario->setId($input['usuario']['_id']);
+        $usuario->setUsername($input['usuario']['_username']);
+        $usuario->setPasword($input['usuario']['_password']);
+        $usuario->setNombre($input['usuario']['_nombre']);
+        $usuario->setApaterno($input['usuario']['_apaterno']);
+        $usuario->setAmaterno($input['usuario']['_amaterno']);
+        $usuario->setFechaNac($input['usuario']['_fechaNac']);
+        $usuario->setMail($input['usuario']['_mail']);
+        $usuario->setTipo($input['usuario']['_tipo']);
+        $usuario->setAs($input['usuario']['_as']);
+    }
+
+    switch($accion){
+        case 'crearUsuario':
+            $result = $usuario->crearUsuario();
+            break;
+        case 'validarUsuario':
+            $result = $usuario->validarUsuario();
+            break;
+        default:
+            break;
+    }
+
+    if(!$result){
+        $json = "No se encontro ninguna accion";
+    }else{
+        $json = $result;
+    }
+
+} catch (\Throwable $th) {
+    $json = "Error en index: ".$th; 
+}
+echo json_encode($json);
+?>
---
+++ usuario/usuario.php
@@ -0,0 +1,156 @@
+<?php
+class Usuario
+{
+    private $id;
+    private $usuarname;
+    private $password;
+    private $nombre;
+    private $apaterno;
+    private $amaterno;
+    private $fechaNac;
+    private $mail;
+    private $tipo;
+    private $as;
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function getUsername()
+    {
+        return $this->usuarname;
+    }
+    public function setUsername($usuarname)
+    {
+        $this->usuarname = $usuarname;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function setPasword($password)
+    {
+        $this->password = $password;
+    }
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+    public function getApaterno()
+    {
+        return $this->apaterno;
+    }
+    public function setApaterno($apaterno)
+    {
+        $this->apaterno = $apaterno;
+    }
+    public function getAmaterno()
+    {
+        return $this->amaterno;
+    }
+    public function setAmaterno($amaterno)
+    {
+        $this->amaterno = $amaterno;
+    }
+    public function getFechaNac()
+    {
+        return $this->fechaNac;
+    }
+    public function setFechaNac($fechaNac)
+    {
+        $this->fechaNac = $fechaNac;
+    }
+    public function getMail()
+    {
+        return $this->mail;
+    }
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+    public function getAs()
+    {
+        return $this->as;
+    }
+    public function setAs($as)
+    {
+        $this->as = $as;
+    }
+    //-------------------------------------------------------------------------
+//              crear usuario
+//-------------------------------------------------------------------------
+    function crearUsuario()
+    {
+        $conn = conexionMSQLI();
+        $password = md5($this->password);
+
+        try {
+            $sql = $conn->prepare("INSERT into usuarios values (null,?,?,?,?,?,?,?,1,1,1)");
+            $sql->bind_param('sssssss', $this->usuarname, $password, $this->nombre, $this->apaterno, $this->amaterno, $this->fechaNac, $this->mail);
+            $result = $sql->execute();
+
+            $conn->close();
+
+            return "Registro exitoso";
+
+        } catch (\Throwable $th) {
+            return false;
+        }
+
+    }
+    //-------------------------------------------------------------------------
+//              Validar usuario
+//-------------------------------------------------------------------------
+    public function validarUsuario()
+    {
+
+        try {
+            $conex = conexionMSQLI();
+
+            $sql = "SELECT * from usuarios where username = '$this->usuarname'";
+            $result = mysqli_query($conex, $sql);
+            $json = array();
+
+            if (mysqli_num_rows($result)) {
+                if ($row = mysqli_fetch_array($result)) {
+                    if (md5($this->password) == $row["password"]) {
+                        $json["flag"] = true;
+                        $json["id"] = $row["id"];
+                        $json["msg"] = "Verificacion Exitosa ";
+                    } else {
+                        $json["flag"] = false;
+                        $json["msg"] = "Usuario o contraseña incorrectos";
+                    }
+                }
+            } else {
+                $json["flag"] = false;
+                $json["msg"] = "Usuario o contraseña incorrectos";
+            }
+
+            $conex->close();
+
+            return $json;
+
+        } catch (\Throwable $e) {
+            //throw $th;
+            $json["msg"] = "Error en el servidor".$e;
+            return $json;
+        }
+    }
+}
+?>
